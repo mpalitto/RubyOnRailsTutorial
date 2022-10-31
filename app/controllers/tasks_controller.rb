dde0066a2cac35c4688f642d2e1d2fd94b594118
Prof_Palitto Palitto <mpalitto@gmail.com>
2022-10-31 20:39:04 +0000

    destroy method not working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,20 @@ class TasksController < ApplicationController
 def new
   @task = Task.new
 end
+
 def create
   @task = Task.create(task_params)
-  redirect_to root_path
+  @tasks = Task.all
 end
 private
 def task_params
   params.require(:task).permit(:title, :note, :completed)
 end
+
+def destroy
+#  @task = Task.find(params[:id])
+#  @task.destroy
+  @tasks = Task.all
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
  root "pages#home"
  resources :tasks
- #get "tasks/new" => 'tasks#new', :as => :new
 
 end
